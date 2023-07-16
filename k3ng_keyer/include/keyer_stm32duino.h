6f598400eb1ccc84749abd2fc27bb713c41265ff
Jeff Rizzo <riz@tastylime.net>
2023-07-16 15:11:53 -0700

    Make bluepill buildable with PlatformIO
    Built here with:
    pio run -e bluepill
    
    Compiled only, untested on hardware

--- k3ng_keyer/include/keyer_stm32duino.h
+++ k3ng_keyer/include/keyer_stm32duino.h
@@ -34,6 +34,4 @@
   void service_send_buffer(byte);
   void check_ptt_tail(void);
   void check_for_dirty_configuration(void);
-  void tone(uint32_t, uint32_t, uint32_t);
-  void noTone(uint32_t);
   void serial_status(PRIMARY_SERIAL_CLS*);
